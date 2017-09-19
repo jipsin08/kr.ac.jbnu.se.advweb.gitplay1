4591d0f13eae925b027e740229f181133bedcfea
Ex <Ex@DESKTOP-LOHG76H>
2017-09-19 10:47:02 +0900

    please, make confliction on it.

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
@@ -5,6 +5,6 @@ public class HelloGit4 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		
-		System.out.println("confliction was occured");
+		System.out.println("what up kimjinlee");
 	}
 }
