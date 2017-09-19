3e97bd23db3be18f620b387b0eb881ef8d510772
MomsToucha <shak5643@gmail.com>
2017-09-19 10:46:19 +0900

    confliction restrict

--- src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
+++ src/kr/ac/jbnu/se/advweb/gitplay0/HelloGit4.java
@@ -5,6 +5,6 @@ public class HelloGit4 {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		
-		System.out.println("confliction was occured");
+		System.out.println("No, i'm gonna stop this.");
 	}
 }
